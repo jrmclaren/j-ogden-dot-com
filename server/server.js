ebda9b8491514f15a9461f716388e2ba91b8e0f4
Jason McLaren <jmclar@ext.uber.com>
2018-04-05 15:54:36 +1000

    updated server to use port variable for heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = new express();
+const port = process.env.PORT || 3000;
 var static = express.static('build');
 
 app.use(static);
-app.listen(9080);
+app.listen(port);
